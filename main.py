87cc17f294852f63f90091bc8fd3986adc48c246
Desgue <guedesgomero@gmail.com>
2024-03-19 12:31:31 +0000

    feat: add logger

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import os
 import time
 import argparse
-
 from sync import Synchronizer
+import logging
+import sys
 """ Naive Solution """
 
 
@@ -15,6 +16,16 @@ def configure_parser() -> argparse.ArgumentParser:
     parser.add_argument("-l", "--log", help="Log file path.", required=True, type=str)
     parser.add_argument("-i", "--interval", help="Interval in seconds. Default is 60s.", required=False, default=60, type=int)
     return parser
+def configure_logger(log_path: str, log_name) -> logging.Logger:
+    """ Configure the logger """
+    logger = logging.getLogger(log_name)
+    logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.INFO)
+    stdout_handler.setFormatter(logging.Formatter("\n%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(stdout_handler)
+    return logger
 
 def main() -> None:
     """ Main function """
@@ -23,24 +34,23 @@ def main() -> None:
     source = Path(args.source)
     backup = Path(args.backup)
     log = Path(args.log)
+    logger = configure_logger(log_path=log, log_name="Sync")
 
     if not source.exists():
-        print("source folder does not exist")
+        print("\nSource folder does not exist")
         return
     if not backup.exists():
-        print("Backup folder does not exist")
+        print("\nBackup folder does not exist")
         return
-    if not log.exists():
-        print("Log file does not exist")
+    if not log.exists() or not log.is_file():
+        print("\nLog file path is invalid")
         return
 
     print(f"Source: {source}\nbackup: {backup}\nInterval: {args.interval}\nLog: {log}")
 
     while True:
-
-        sync = Synchronizer(source, backup)
+        sync = Synchronizer(source, backup, logger)
         sync.synchronize()
-
         time.sleep(args.interval)
 
 if __name__ == "__main__":
--- sync.py
+++ sync.py
@@ -4,10 +4,13 @@ import filecmp
 import os
 
 class Synchronizer:
-    def __init__(self, source, replica):
+    def __init__(self, source, replica, logger):
         self.source = source
         self.replica = replica
-
+        self.logger = logger
+        self.error_message = "Error %s %s -> %s"
+        self.log_message = "%s %s -> %s to %s"
+        self.delete_message = "Deleting %s -> %s"
         
     def __str__(self):
         return f"Source: {self.source}\nReplica: {self.replica}"
@@ -17,52 +20,59 @@ class Synchronizer:
         compared = filecmp.dircmp(self.source, self.replica)
         for item in compared.left_only:
             to_copy = Path(self.source, item)
+            action = "Copying"
             if to_copy.is_file():
-                print(f"\nCopying file-> {to_copy} to {self.replica}\n")
                 try:
                     shutil.copy2(to_copy, Path(self.replica, item))
+                    self.logger.info(self.log_message, action, "file", to_copy, self.replica)
                 except Exception as e:
-                    print(f"Error copying {to_copy} -> {e}")
-
+                    self.logger.error(self.error_message,action, "file", e)
+                    
             if to_copy.is_dir():
-                print(f"\nCopying folder-> {to_copy} to {self.replica}\n")
                 try:
                     shutil.copytree(to_copy, Path(self.replica, item), copy_function=shutil.copy2, dirs_exist_ok=True)
+                    self.logger.info(self.log_message, action, "folder", to_copy, self.replica)
+
                 except Exception as e:
-                    print(f"Error copying {to_copy} -> {e}")
+                    self.logger.error(error_message,action, to_copy, e)
+
+
         
         for item in compared.right_only:
             to_delete = Path(self.replica, item)
+            action = "Deleting"
             if to_delete.is_file():
-                print(f"\nDeleting file-> {to_delete}\n")
                 try:
                     os.unlink(to_delete)
+                    self.logger.info(self.delete_message, "file", to_delete)
+
                 except Exception as e:
-                    print(f"Error deleting {to_delete} -> {e}")
+                    self.logger.error(self.error_message, action, "file", to_delete, e)
 
             if to_delete.is_dir():
-                print(f"\nDeleting folder-> {to_delete}\n")
                 try:
                     shutil.rmtree(to_delete)
+                    self.logger.info(self.delete_message, "folder", to_delete)
+
                 except Exception as e:
-                    print(f"Error deleting {to_delete} -> {e}") 
+                    self.logger.error(self.error_message, action, "folder", to_delete, e)
 
         for item in compared.diff_files:
             to_copy = Path(self.source, item)
+            action = "Copying"
             if to_copy.is_file():
-                print(f"\nCopying file-> {to_copy} to {self.replica}\n")
                 try:
                     shutil.copy2(to_copy, Path(self.replica, item))
+                    self.logger.info(self.log_message, action, "file", to_copy, self.replica)
                 except Exception as e:
-                    print(f"Error copying {to_copy} -> {e}")
+                    self.logger.error(f"Error copying {to_copy} -> {e}")
             if to_copy.is_dir():
-                print(f"\nCopying folder-> {to_copy} to {self.replica}\n")
                 try:
                     shutil.copytree(to_copy, Path(self.replica, item), copy_function=shutil.copy2, dirs_exist_ok=True)
+                    self.logger.info(self.log_message, action, "folder", to_copy, self.replica)
                 except Exception as e:
-                    print(f"Error copying {to_copy} -> {e}")
+                    self.error_message(self.error_message, action, "folder", to_copy, e)
 
         for item in compared.common_dirs:
-            print(f"\nSynching {item}\n")
-            Synchronizer(self.source.joinpath(item), self.replica.joinpath(item)).synchronize()
+            Synchronizer(self.source.joinpath(item), self.replica.joinpath(item), self.logger).synchronize()
 
